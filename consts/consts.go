5b865ec12faee88c1f31bbf1eed4946a9f2fcb83
Jeri-Michael Joseph Advincula <swordfartonline11@gmail.com>
2023-11-10 21:55:28 +0800

    push

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ron"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "solanacoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SC"
 )
 
 var ID ids.ID
